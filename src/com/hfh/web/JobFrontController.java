99cef5bc6108d02e70f7ab86a4b43a39620221a6
NuiStrawBerry <jonasyang.lover@gmail.com>
2014-11-04 23:36:40 +0800

    change  offers in aboutus return ajax load the html to show the info

--- src/com/hfh/web/JobFrontController.java
+++ src/com/hfh/web/JobFrontController.java
@@ -1,15 +1,11 @@
 package com.hfh.web;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.hfh.bean.Job;
@@ -21,21 +17,12 @@ public class JobFrontController {
 	@Autowired
 	JobService jobService;
 	  
-	  @RequestMapping(value="/offers",method = RequestMethod.GET)
-	    public String getOffers(@RequestParam("id") String id, Model model){
-	       List<Job> jobs= jobService.getAllJobs();
-	        model.addAttribute(jobs);//("houseInfo",result);
-	       
-	        return "jobs";
+	  @RequestMapping(value="/offers",method = RequestMethod.POST)
+	  @ResponseBody
+	  public  List<Job> getOffers(){
+	       List<Job> jobs= jobService.getJobs();
+	        return jobs;
 	    }
 	  
-	  
-	  /*  @RequestMapping(value="/fhouseinfodetail",method = RequestMethod.GET)
-	    @ResponseBody
-	    public Map<String,HouseInfo> getHouseInfoDetail(@RequestParam("id") String id ){
-	        Map<String,HouseInfo> r = new HashMap<String,HouseInfo>();
-	       // HouseInfo result = houseManager.getHouseInfo(id);
-	       // r.put("houseInfo",result);
-	        return r;
-	    }*/
+	 
 }
