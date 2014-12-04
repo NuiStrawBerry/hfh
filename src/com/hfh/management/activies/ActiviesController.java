516cdfa91c5aa4a300b2d5d6ba9edcf5d9851bea
NuiStrawBerry <jonasyang.lover@gmail.com>
2014-12-04 19:49:46 +0800

    as

--- src/com/hfh/management/activies/ActiviesController.java
+++ src/com/hfh/management/activies/ActiviesController.java
@@ -1,18 +1,14 @@
 package com.hfh.management.activies;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 
