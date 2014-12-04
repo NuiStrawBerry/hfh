a1b4dad479482043ebca112da1bdac8fe2be8152
NuiStrawBerry <jonasyang.lover@gmail.com>
2014-12-04 19:48:25 +0800

    add operate system condition

--- src/com/hfh/util/PathUtils.java
+++ src/com/hfh/util/PathUtils.java
@@ -1,13 +1,17 @@
 package com.hfh.util;
 
-import java.io.File;
-
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 @Service
 public class PathUtils {
 	
+//	private final boolean osName = System.getProperty("os.name")
+//	private String path = "";
+	
 	final String path="F:\\java plugin\\apache-tomcat-6.0.36\\wtpwebapps\\hfh\\timgs\\"; 
-	//final String linuxPath = "/opt/tomcat7/webapps/ROOT/timgs/";
+//	final String path = "/opt/tomcat7/webapps/ROOT/timgs/";
 	public String getUploadPath(){
 		String separator = System.getProperty("file.separator");
     	StringBuilder sb = new StringBuilder(System.getProperty("user.dir"));
@@ -16,7 +20,22 @@ public class PathUtils {
     	if(!f.exists()){
     		f.mkdirs();
     	}
-    	return path;
+    	return getPath();
 	}
+	
+	
+	public String getOsName(){
+		
+		return System.getProperty("os.name");
+	}
+	
+	public String getPath(){
+		 String path = "/opt/tomcat7/webapps/ROOT/timgs/";
+		if(StringUtils.isNotBlank(getOsName()) && getOsName().startsWith("Windows")){
+			path = "F:\\java plugin\\apache-tomcat-6.0.36\\wtpwebapps\\hfh\\timgs\\";
+		}
+		return path;
+	}
+	
 
 }
