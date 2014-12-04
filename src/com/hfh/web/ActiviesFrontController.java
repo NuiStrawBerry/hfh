49e39f605f5074ca7501986f015b69dbbe2a5067
NuiStrawBerry <jonasyang.lover@gmail.com>
2014-12-04 19:47:53 +0800

    dont no

--- src/com/hfh/web/ActiviesFrontController.java
+++ src/com/hfh/web/ActiviesFrontController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.hfh.bean.HouseInfo;
 import com.hfh.management.activies.Activies;
 import com.hfh.management.activies.ActiviesService;
 import com.hfh.util.ListPageModel;
@@ -38,5 +38,17 @@ public class ActiviesFrontController {
 			return page;
 	    }
 	  
+	  @RequestMapping(value="/activiesDetail",method = RequestMethod.POST)
+	  @ResponseBody
+	  public Map<String,Object> acDetail(@RequestParam("params") String id,
+				@RequestParam(value="where",required=false) String where){
+		  Map<String,Object> result = new HashMap<String,Object>();
+		  Activies  activies = activiesService.getActivies(id);
+		 result.put("activies", activies);
+		 if(StringUtils.isNotBlank(where)){
+			 result.put("where", "home");
+		 }
+			return result;
+	    }
 	 
 }
--- src/com/hfh/web/TopHouseInfoController.java
+++ src/com/hfh/web/TopHouseInfoController.java
@@ -1,33 +1,29 @@
 package com.hfh.web;
 
-import java.io.IOException;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.hfh.bean.HouseInfo;
-import com.hfh.management.houseinfo.service.HouseManager;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.stereotype.Controller ;
+
+import com.hfh.bean.HouseInfo;
+import com.hfh.management.activies.Activies;
+import com.hfh.management.activies.ActiviesService;
+import com.hfh.management.houseinfo.service.HouseManager;
 @Controller
 public class TopHouseInfoController {
 
     @Autowired
     private HouseManager houseManager;
+    
+    @Autowired
+	ActiviesService activiesService;
+    
 
     @RequestMapping(value="/tophouseInfos",method = RequestMethod.POST)
     @ResponseBody
@@ -60,13 +56,17 @@ public class TopHouseInfoController {
     
     @RequestMapping(value="/activies",method = RequestMethod.POST)
     @ResponseBody
-    public void getActivity(){
+    public Map<String,List<Activies>> getActivity(){
+    	 Map<String,List<Activies>> r = new HashMap<String,List<Activies>>();
+    	 List<Activies> result = activiesService.gethomeAc();;
+         r.put("acInfoList",result);
+         return r;
     	
     }
     
     @RequestMapping(value="/cooperater",method = RequestMethod.POST)
     @ResponseBody
     public void getCooperater(){
-    	
+    
     }
 }
